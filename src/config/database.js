edd234eba25052faff4d4fe1d1b4155a6163a82c
Cleciana <cleciana.santana@ccc.ufcg.edu.br>
2021-11-05 15:06:05 -0300

    postgres add-on

--- src/config/database.js
+++ src/config/database.js
@@ -10,5 +10,16 @@ module.exports = {
     "define": {
       "timestamps":true
     }
+  },
+  "production": {
+    "username": process.env.PGUSER,
+    "password": process.env.PGPASSWORD,
+    "database": process.env.DATABASE,
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "use_env_variable": "DATABASE_URL",
+    "define": {
+      "timestamps":true
+    }
   }
 }
