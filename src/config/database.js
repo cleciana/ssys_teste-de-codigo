8effb325813caecbfdfcc519a8693db24719b166
Cleciana <cleciana.santana@ccc.ufcg.edu.br>
2021-11-05 21:09:04 -0300

    heroku config

--- src/config/database.js
+++ src/config/database.js
@@ -5,19 +5,21 @@ module.exports = {
     "username": process.env.PGUSER,
     "password": process.env.PGPASSWORD,
     "database": process.env.DATABASE,
-    "host": "127.0.0.1",
+    "host": "localhost",
     "dialect": "postgres",
     "define": {
       "timestamps":true
     }
   },
   "production": {
-    "username": process.env.PGUSER,
-    "password": process.env.PGPASSWORD,
-    "database": process.env.DATABASE,
-    "host": "127.0.0.1",
-    "dialect": "postgres",
     "use_env_variable": "DATABASE_URL",
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false 
+      }
+    },
     "define": {
       "timestamps":true
     }
